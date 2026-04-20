cef63cb9b295b5bffd2c4e6d91f60771db5e297e
leducminh85 <ldminh852001@gmail.com>
2026-04-20 12:54:35 +0700

    feat: update frontend to support local database searching

--- nextjs-app/next.config.mjs
+++ nextjs-app/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  /* Standalone output for Docker deployment */
+  output: 'standalone',
 };
 
 export default nextConfig;
